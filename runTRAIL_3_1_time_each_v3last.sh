a0e4b14f8db4ed3404c69f8c057581534757cad7
janunezg <103081788+janunezg@users.noreply.github.com>
2025-02-05 12:39:40 -0300

    f

--- runTRAIL_3_1_time_each_v3last.sh
+++ runTRAIL_3_1_time_each_v3last.sh
@@ -10,23 +10,6 @@ PATTERNS=(
 
 
 
-"MATCH (m2597148)=[ALL TRAILS ?p1 (:hasCreator)/:knows{0,5}]=>(?y) RETURN COUNT(?p1)"
-"MATCH (f54)=[ALL TRAILS ?p1 (:hasMember)/:knows{0,5}]=>(?y) RETURN COUNT(?p1)"
-"MATCH (f6)=[ALL TRAILS ?p1 (:hasModerator)/:knows{0,5}]=>(?y) RETURN COUNT(?p1)"
-"MATCH (t6)=[ALL TRAILS ?p1 (:hasType)/:isSubclassOf{0,5}]=>(?y) RETURN COUNT(?p1)"
-"MATCH (tc7)=[ALL TRAILS ?p1 (:isSubclassOf)/:isSubclassOf{0,5}]=>(?y) RETURN COUNT(?p1)"
-"MATCH (p32)=[ALL TRAILS ?p1 (:knows)/:knows{0,5}]=>(?y) RETURN COUNT(?p1)"
-"MATCH (p20956)=[ALL TRAILS ?p1 (:likes)/:replyOf{0,5}]=>(?y) RETURN COUNT(?p1)"
-"MATCH (m2597159)=[ALL TRAILS ?p1 (:replyOf)/:replyOf{0,5}]=>(?y) RETURN COUNT(?p1)"
-
-
-"MATCH (f60)=[ALL TRAILS ?p1 (:containerOf)|:replyOf{1,5}]=>(?y) RETURN COUNT(?p1)"
-"MATCH (m2597148)=[ALL TRAILS ?p1 (:hasCreator)|:knows{1,5}]=>(?y) RETURN COUNT(?p1)"
-"MATCH (f54)=[ALL TRAILS ?p1 (:hasMember)|:knows{1,5}]=>(?y) RETURN COUNT(?p1)"
-"MATCH (f6)=[ALL TRAILS ?p1 (:hasModerator)|:knows{1,5}]=>(?y) RETURN COUNT(?p1)"
-"MATCH (t6)=[ALL TRAILS ?p1 (:hasType)|:isSubclassOf{1,5}]=>(?y) RETURN COUNT(?p1)"
-"MATCH (tc7)=[ALL TRAILS ?p1 (:isSubclassOf)|:isSubclassOf{1,5}]=>(?y) RETURN COUNT(?p1)"
-"MATCH (p32)=[ALL TRAILS ?p1 (:knows)|:knows{1,5}]=>(?y) RETURN COUNT(?p1)"
 "MATCH (p20956)=[ALL TRAILS ?p1 (:likes)|:replyOf{1,5}]=>(?y) RETURN COUNT(?p1)"
 "MATCH (m2597159)=[ALL TRAILS ?p1 (:replyOf)|:replyOf{1,5}]=>(?y) RETURN COUNT(?p1)"
 
