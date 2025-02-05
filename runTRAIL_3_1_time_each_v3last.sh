e7dbace22cb0196062f7216225507c535a870aec
janunezg <103081788+janunezg@users.noreply.github.com>
2025-02-05 12:03:51 -0300

    asd

--- runTRAIL_3_1_time_each_v3last.sh
+++ runTRAIL_3_1_time_each_v3last.sh
@@ -7,28 +7,60 @@ BASE_URL="http://localhost:1234/query"
 PATTERNS=(
 
 
-"MATCH (m2597148)=[ALL TRAILS ?p1 (:hasCreator ?)?]=>(?y) RETURN COUNT(?p1)"
-"MATCH (f60)=[ALL TRAILS ?p1 (:containerOf ?)?]=>(?y) RETURN COUNT(?p1)"
-"MATCH (p56)=[ALL TRAILS ?p1 (:hasInterest ?)?]=>(?y) RETURN COUNT(?p1)"
-"MATCH (f54)=[ALL TRAILS ?p1 (:hasMember ?)?]=>(?y) RETURN COUNT(?p1)"
-"MATCH (f6)=[ALL TRAILS ?p1 (:hasModerator ?)?]=>(?y) RETURN COUNT(?p1)"
-"MATCH (f389)=[ALL TRAILS ?p1 (:hasTag ?)?]=>(?y) RETURN COUNT(?p1)"
-"MATCH (t6)=[ALL TRAILS ?p1 (:hasType ?)?]=>(?y) RETURN COUNT(?p1)"
-"MATCH (m2597148)=[ALL TRAILS ?p1 (:isLocatedIn ?)?]=>(?y) RETURN COUNT(?p1)"
-"MATCH (pL6)=[ALL TRAILS ?p1 (:isPartOf ?)?]=>(?y) RETURN COUNT(?p1)"
-"MATCH (tc7)=[ALL TRAILS ?p1 (:isSubclassOf ?)?]=>(?y) RETURN COUNT(?p1)"
-"MATCH (p32)=[ALL TRAILS ?p1 (:knows ?)?]=>(?y) RETURN COUNT(?p1)"
-"MATCH (p20956)=[ALL TRAILS ?p1 (:likes ?)?]=>(?y) RETURN COUNT(?p1)"
-"MATCH (m2597159)=[ALL TRAILS ?p1 (:replyOf ?)?]=>(?y) RETURN COUNT(?p1)"
-"MATCH (p8)=[ALL TRAILS ?p1 (:studyAt ?)?]=>(?y) RETURN COUNT(?p1)"
-"MATCH (p9)=[ALL TRAILS ?p1 (:workAt ?)?]=>(?y) RETURN COUNT(?p1)"
+"MATCH (p32)=[ALL TRAILS ?p1 (:knows {1,5})/:studyAt]=>(?y) RETURN COUNT(?p1)"
+"MATCH (p32)=[ALL TRAILS ?p1 (:knows {1,5})/:workAt]=>(?y) RETURN COUNT(?p1)"
+"MATCH (m2597159)=[ALL TRAILS ?p1 (:replyOf {1,5})/:hasCreator]=>(?y) RETURN COUNT(?p1)"
+"MATCH (m2597159)=[ALL TRAILS ?p1 (:replyOf {1,5})/:hasTag]=>(?y) RETURN COUNT(?p1)"
+"MATCH (m2597159)=[ALL TRAILS ?p1 (:replyOf {1,5})/:replyOf]=>(?y) RETURN COUNT(?p1)"
+
+"MATCH (m2597159)=[ALL TRAILS ?p1 (:replyOf {0,5})/:hasCreator]=>(?y) RETURN COUNT(?p1)"
+"MATCH (tc7)=[ALL TRAILS ?p1 (:isSubclassOf {0,5})/:isSubclassOf]=>(?y) RETURN COUNT(?p1)"
+"MATCH (p32)=[ALL TRAILS ?p1 (:knows {0,5})/:isLocatedIn]=>(?y) RETURN COUNT(?p1)"
+"MATCH (p32)=[ALL TRAILS ?p1 (:knows {0,5})/:knows]=>(?y) RETURN COUNT(?p1)"
+"MATCH (p32)=[ALL TRAILS ?p1 (:knows {0,5})/:likes]=>(?y) RETURN COUNT(?p1)"
 
 
+"MATCH (m2597148)=[ALL TRAILS ?p1 (:hasCreator)/:knows{1,5}]=>(?y) RETURN COUNT(?p1)"
+"MATCH (f54)=[ALL TRAILS ?p1 (:hasMember)/:knows{1,5}]=>(?y) RETURN COUNT(?p1)"
+"MATCH (f6)=[ALL TRAILS ?p1 (:hasModerator)/:knows{1,5}]=>(?y) RETURN COUNT(?p1)"
+"MATCH (t6)=[ALL TRAILS ?p1 (:hasType)/:isSubclassOf{1,5}]=>(?y) RETURN COUNT(?p1)"
+"MATCH (tc7)=[ALL TRAILS ?p1 (:isSubclassOf)/:isSubclassOf{1,5}]=>(?y) RETURN COUNT(?p1)"
+"MATCH (p32)=[ALL TRAILS ?p1 (:knows)/:knows{1,5}]=>(?y) RETURN COUNT(?p1)"
+"MATCH (p20956)=[ALL TRAILS ?p1 (:likes)/:replyOf{1,5}]=>(?y) RETURN COUNT(?p1)"
+"MATCH (m2597159)=[ALL TRAILS ?p1 (:replyOf)/:replyOf{1,5}]=>(?y) RETURN COUNT(?p1)"
 
 
+"MATCH (m2597148)=[ALL TRAILS ?p1 (:hasCreator)/:knows{0,5}]=>(?y) RETURN COUNT(?p1)"
+"MATCH (f54)=[ALL TRAILS ?p1 (:hasMember)/:knows{0,5}]=>(?y) RETURN COUNT(?p1)"
+"MATCH (f6)=[ALL TRAILS ?p1 (:hasModerator)/:knows{0,5}]=>(?y) RETURN COUNT(?p1)"
+"MATCH (t6)=[ALL TRAILS ?p1 (:hasType)/:isSubclassOf{0,5}]=>(?y) RETURN COUNT(?p1)"
+"MATCH (tc7)=[ALL TRAILS ?p1 (:isSubclassOf)/:isSubclassOf{0,5}]=>(?y) RETURN COUNT(?p1)"
+"MATCH (p32)=[ALL TRAILS ?p1 (:knows)/:knows{0,5}]=>(?y) RETURN COUNT(?p1)"
+"MATCH (p20956)=[ALL TRAILS ?p1 (:likes)/:replyOf{0,5}]=>(?y) RETURN COUNT(?p1)"
+"MATCH (m2597159)=[ALL TRAILS ?p1 (:replyOf)/:replyOf{0,5}]=>(?y) RETURN COUNT(?p1)"
+
+
+"MATCH (f60)=[ALL TRAILS ?p1 (:containerOf)|:replyOf{1,5}]=>(?y) RETURN COUNT(?p1)"
+"MATCH (m2597148)=[ALL TRAILS ?p1 (:hasCreator)|:knows{1,5}]=>(?y) RETURN COUNT(?p1)"
+"MATCH (f54)=[ALL TRAILS ?p1 (:hasMember)|:knows{1,5}]=>(?y) RETURN COUNT(?p1)"
+"MATCH (f6)=[ALL TRAILS ?p1 (:hasModerator)|:knows{1,5}]=>(?y) RETURN COUNT(?p1)"
+"MATCH (t6)=[ALL TRAILS ?p1 (:hasType)|:isSubclassOf{1,5}]=>(?y) RETURN COUNT(?p1)"
+"MATCH (tc7)=[ALL TRAILS ?p1 (:isSubclassOf)|:isSubclassOf{1,5}]=>(?y) RETURN COUNT(?p1)"
+"MATCH (p32)=[ALL TRAILS ?p1 (:knows)|:knows{1,5}]=>(?y) RETURN COUNT(?p1)"
+"MATCH (p20956)=[ALL TRAILS ?p1 (:likes)|:replyOf{1,5}]=>(?y) RETURN COUNT(?p1)"
+"MATCH (m2597159)=[ALL TRAILS ?p1 (:replyOf)|:replyOf{1,5}]=>(?y) RETURN COUNT(?p1)"
+
+
+"MATCH (m2597159)=[ALL TRAILS ?p1 (:replyOf){1,5}]=>(?y) RETURN COUNT(?p1)"
+"MATCH (tc7)=[ALL TRAILS ?p1 (:isSubclassOf){1,5}]=>(?y) RETURN COUNT(?p1)"
+"MATCH (p32)=[ALL TRAILS ?p1 (:knows){1,5}]=>(?y) RETURN COUNT(?p1)"
+
+"MATCH (m2597159)=[ALL TRAILS ?p1 (:replyOf){0,5}]=>(?y) RETURN COUNT(?p1)"
+"MATCH (tc7)=[ALL TRAILS ?p1 (:isSubclassOf){0,5}]=>(?y) RETURN COUNT(?p1)"
+"MATCH (p32)=[ALL TRAILS ?p1 (:knows){0,5}]=>(?y) RETURN COUNT(?p1)"
 
 )
-#recuerda que * es {0,4} y + es {1,4}
+#recuerda que * es {0,4} y + es {1,5}
 # Ejecutar las consultas
 
 for PATTERN in "${PATTERNS[@]}"; do
